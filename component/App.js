41a0fe5efe7c634d1397d6e6498742d6996bb63a
Huy <tranthanhhuy19032001.ltp@gmail.com>
2023-02-08 00:16:46 +0700

    first commit

---
+++ component/App.js
@@ -0,0 +1,17 @@
+import html from '../core.js'
+import { connect } from '../store.js'
+import Header from './Header.js'
+import ToDoList from './ToDoList.js'
+import Footer from './Footer.js'
+
+function App({ todos }) {
+    return html`
+        <section class="todoapp">
+            ${Header()}
+            ${todos.length > 0 && ToDoList()}
+            ${todos.length > 0 && Footer()}
+        </section>
+    `
+}
+
+export default connect()(App)
